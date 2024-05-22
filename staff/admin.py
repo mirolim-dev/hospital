9ac9b1e963dc0478acb5645eb961aa5e07934718
mrdjangodev <mirolimcoder@gmail.com>
2024-05-22 08:24:32 +0500

    Displaying Staff model on admin panel done

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Staff
+from .forms import StaffAdminForm
 # Register your models here.
+
+class StaffAdmin(admin.ModelAdmin):
+    form = StaffAdminForm
+    list_display = ['first_name', 'last_name', 'phone', 'address', 'role', 'is_working']
+    search_fields = ['first_name', 'last_name', 'phone']
+    list_filter = ['role', 'is_working']
+
+admin.site.register(Staff, StaffAdmin)
