5c64684b817daedc6cacb2c68b75a2ace8e86048
mrdjangodev <mirolimcoder@gmail.com>
2024-05-22 08:44:49 +0500

    Custom filter created and applayed on Attandance admin displation

--- staff/admin.py
+++ staff/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Staff
+from .models import Staff, Attandace
 from .forms import StaffAdminForm
 # Register your models here.
 
@@ -10,3 +10,15 @@ class StaffAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name', 'phone']
     list_filter = ['role', 'is_working']
 admin.site.register(Staff, StaffAdmin)
+
+
+class AttandanceAdmin(admin.ModelAdmin):
+    list_display = ['staff', 'get_staff_role', 'tracked_at']
+    search_fields = ['staff__first_name', 'staff__last_name']
+    list_filter = ['staff__role']
+
+    def get_staff_role(self, obj):
+        return obj.staff_id.display_role()
+    get_staff_role.short_description = 'Xodim turi'
+
+admin.site.register(Attandace, AttandanceAdmin)
