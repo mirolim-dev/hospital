e0de381d8c41ac19ff68c92bb9cf230ed4c0ac91
mrdjangodev <mirolimcoder@gmail.com>
2024-05-28 06:32:48 +0500

    static and medifiles settings fixed

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,8 @@ EXCLUDE_PATHS = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media/'
 # Default primary key field type
