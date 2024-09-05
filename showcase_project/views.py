3c897d8c037f2fd378c5183fa93116343a1f8d78
mirolim-dev <mirolimcoder@gmail.com>
2024-09-05 15:35:56 +0500

    project detail page's view is ready

--- showcase_project/views.py
+++ showcase_project/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+# from django.core 
+from .models import AboutProject, Feature, SubFeature
 # Create your views here.
 
 def about(request):
     
-    return render(request, 'index.html')
+    project_detail = None
+    try:
+        project_detail = AboutProject.objects.get(is_active=True)
+    except:
+        project_detail = AboutProject.objects.last()
+    context = {
+        'project_detail': project_detail,
+    }
+    return render(request, 'index.html', context)
